52091c3f5ffd5520212bf499f1b77f1936b5794a
John McQueen <mcqueenjpg@gmail.com>
2014-08-29 14:08:40 -0700

    Initial project commit

---
+++ README.md
@@ -0,0 +1,4 @@
+fw-maps
+===========
+
+Mapping of Projects by Faculty and Students in the Department of Fisheries and Wildlife at Oregon State University
---
+++ map.js
@@ -0,0 +1,37 @@
+L.mapbox.accessToken = 'pk.eyJ1IjoiamZ1enp5IiwiYSI6ImxCMkF5NVUifQ.Zg6RDpdbQ-7WGd2C_niK7g';
+
+var map = L.mapbox.map('map', 'jfuzzy.jc52jpo4')
+  .setView([44.7077, -122.0471], 3);
+
+var layers = document.getElementById('menu-ui');
+
+addLayer(L.mapbox.featureLayer('fw-shelby.jb9il5be'), 'Active Projects', 2);
+addLayer(L.mapbox.featureLayer('fw-shelby.jba8bnf6'), 'Past Projects', 3);
+
+function addLayer(layer, name, zIndex) {
+    layer
+        .setZIndex(zIndex)
+        .addTo(map);
+
+    // Create a simple layer switcher that
+    // toggles layers on and off.
+    var link = document.createElement('a');
+        link.href = '#';
+        link.className = 'active';
+        link.innerHTML = name;
+
+    link.onclick = function(e) {
+        e.preventDefault();
+        e.stopPropagation();
+
+        if (map.hasLayer(layer)) {
+            map.removeLayer(layer);
+            this.className = '';
+        } else {
+            map.addLayer(layer);
+            this.className = 'active';
+        }
+    };
+
+    layers.appendChild(link);
+}
